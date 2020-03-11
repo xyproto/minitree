e607768a1dd5b0b0aae0cce88e96d7f92003c07b
Alexander F. Rødseth <rodseth@gmail.com>
2020-03-11 11:42:34 +0100

    Project rename

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
-lq
-==
+# minitree
 
 List the files in the directories in the current directory, formatted as columns.
 
+Screenshot from an older version:
+
 ![screenshot](screenshot.png)
+
+### Background
 
 Inspired by [this question](http://unix.stackexchange.com/questions/83072/ls-should-display-contents-of-flat-directory-structure-in-columns).
 
-Tested with Python 2.6, 2.7, 3.3 and 3.5.
+### Supported Python versions
 
-General info
-------------
+* Tested with Python 2.6, 2.7, 3.3, 3.5 and 3.8. I assume it will work on most versions.
 
-* Version: 0.3
+### General info
+
+* Version: 0.4.0
 * License: MIT
 * Author: Alexander F Rødseth &lt;xyproto@archlinux.org&gt;
 
---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+from setuptools import setup
+
+setup(name="minitree",
+      version="0.4.0",
+      description="List files in columns",
+      url="https://github.com/xyproto/minitree",
+      author="Alexander F. Rødseth",
+      author_email="xyproto@archlinux.org",
+      license="MIT",
+      py_modules=["minitree"],
+      entry_points={
+        "console_scripts" : [
+            "mt = minitree:main",
+        ]
+      },
+      classifiers=[
+          "Environment :: Console",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python",
+          "Topic :: System :: Shells",
+          "Topic :: Utilities",
+      ]
+)
